157ee2ed60350862402409a81711df6558234474
Anderson Tagata <admin@attagata.com>
2017-05-18 16:22:27 -0300

    # ATT 2017-05-18 v10.0.59.2 - Added Logging resource / Converting of Tab to Space Identation.

---
+++ logging_config.py
@@ -0,0 +1,29 @@
+import os
+import json
+import logging.config
+
+#ATT 2017-05-11
+#
+#Esse modulo nao requer personalizacao
+# Use o arquivo logging.json para personalizar a configuracao.
+
+#This module doesn't require any custom changes
+# Use the file logginh.json for any customize on configuration.
+
+def setup_logging(
+    default_path='logging_config.json',
+    default_level=logging.INFO,
+    env_key='LOG_CFG'
+    ):
+    #Setup logging configuration
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
