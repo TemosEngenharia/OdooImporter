034c1f44c36e0a55b72d97ce31b146f781ccaac0
cezarsantanna <cezar.santanna@gmail.com>
2017-06-23 11:59:23 -0300

    Change Username

--- conf/odoo_importer_from_xml_cxsd_config.py
+++ conf/odoo_importer_from_xml_cxsd_config.py
@@ -8,74 +8,73 @@
 #
 # SETTINGS
 #
-#Logging Required Imports
-from . import logging_config as lc
+# Logging Required Imports
 import logging
 
+from . import logging_config as lc
 from .db_config import odoo_config
 
 
 try:
-  from lxml import etree
-  print("running with lxml.etree")
+    from lxml import etree
+    print("running with lxml.etree")
 except ImportError:
-  try:
-    # Python 2.5
-    import xml.etree.cElementTree as etree
-    print("running with cElementTree on Python 2.5+")
-  except ImportError:
     try:
-      # Python 2.5
-      import xml.etree.ElementTree as etree
-      print("running with ElementTree on Python 2.5+")
+        # Python 2.5
+        import xml.etree.cElementTree as etree
+        print("running with cElementTree on Python 2.5+")
     except ImportError:
-      try:
-        # normal cElementTree install
-        import cElementTree as etree
-        print("running with cElementTree")
-      except ImportError:
         try:
-          # normal ElementTree install
-          import elementtree.ElementTree as etree
-          print("running with ElementTree")
+            # Python 2.5
+            import xml.etree.ElementTree as etree
+            print("running with ElementTree on Python 2.5+")
         except ImportError:
-          print("Failed to import ElementTree from any known place")
+            try:
+                # normal cElementTree install
+                import cElementTree as etree
+                print("running with cElementTree")
+            except ImportError:
+                try:
+                    # normal ElementTree install
+                    import elementtree.ElementTree as etree
+                    print("running with ElementTree")
+                except ImportError:
+                    print("Failed to import ElementTree from any known place")
 
 def init(arg):
-  logger = logging.getLogger(__name__)
-	
-  global settings
-  settings = []
-  settings.append(arg)
+    logger = logging.getLogger(__name__)
+    global settings
+    settings = []
+    settings.append(arg)
 
-  global sourceFilenameFieldName
-  global inputXMLPath, inputXMLFileName, inputXMLFile
-  global inputCXSDPath, inputCXSDFileName, inputCXSDFile
+    global sourceFilenameFieldName
+    global inputXMLPath, inputXMLFileName, inputXMLFile
+    global inputCXSDPath, inputCXSDFileName, inputCXSDFile
 
-  #inputXMLPath = "../OdooImporterData/corretiva_v20/xml/" 
-  #inputXMLFileName = "corretiva_v20_20170110-121749.xml"
-  #inputXMLFile = inputXMLPath + inputXMLFileName
+    # inputXMLPath = "../OdooImporterData/corretiva_v20/xml/"
+    # inputXMLFileName = "corretiva_v20_20170110-121749.xml"
+    # inputXMLFile = inputXMLPath + inputXMLFileName
 
-  #inputCXSDPath = "../OdooImporterData/corretiva/schemas/"
-  #inputCXSDFileName = "COR20.cxsd"
-  #inputCXSDFile = inputCXSDPath + inputCXSDFileName
+    # inputCXSDPath = "../OdooImporterData/corretiva/schemas/"
+    # inputCXSDFileName = "COR20.cxsd"
+    # inputCXSDFile = inputCXSDPath + inputCXSDFileName
 
-  #custom global var
-  global main_id
-  global formDateTime
+    # custom global var
+    global main_id
+    global formDateTime
 
-  global odooConfigDict
-  odooConfigDict = odoo_config() #comes from db_tools + db_config.ini
+    global odooConfigDict
+    odooConfigDict = odoo_config()  # comes from db_tools + db_config.ini
 
-  global outputORMPath, outputORMFileName, outputORMFile
-  #outputORMPath = "../../odoo-dev/custom-addons/mcorretiva/models/"
-  #outputORMFileName = "mcorretiva_model.py"
-  #outputORMFile = outputORMPath + outputORMFileName
+    global outputORMPath, outputORMFileName, outputORMFile
+    # outputORMPath = "../../odoo-dev/custom-addons/mcorretiva/models/"
+    # outputORMFileName = "mcorretiva_model.py"
+    # outputORMFile = outputORMPath + outputORMFileName
 
-  global outputORMPath_InDataDomain, outputORMFileName_InDataDomain, outputORMFile_InDataDomain
-  outputORMPath_InDataDomain = "../OdooImporter/data/mco/models/"
-  outputORMFileName_InDataDomain = "mco_model.py"
-  outputORMFile_InDataDomain = outputORMPath_InDataDomain + outputORMFileName_InDataDomain
+    global outputORMPath_InDataDomain, outputORMFileName_InDataDomain, outputORMFile_InDataDomain
+    outputORMPath_InDataDomain = "../OdooImporter/data/mco/models/"
+    outputORMFileName_InDataDomain = "mco_model.py"
+    outputORMFile_InDataDomain = outputORMPath_InDataDomain + outputORMFileName_InDataDomain
 
-  
+
 
